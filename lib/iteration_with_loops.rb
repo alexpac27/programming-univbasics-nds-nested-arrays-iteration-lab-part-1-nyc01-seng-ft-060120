b3c2a06de77525b9314c21050e797bd8b2bedd41
Alexandra Ortiz <alexpac27@gmail.com>
2020-05-26 19:39:29 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,22 +6,5 @@ array = [
 
 
 def find_even_values(array)
-  count = 0
-  even_array = []
-
-  while count < array.length do
-
-    inner_count = 0
-    while inner_count < array[count].length do
-      if array[count][inner_count] %2 == 0
-        even_array << array[count][inner_count]
-        else
-          even_array = even_array
-        end
-        inner_count += 1
-    end
-count += 1
-  end
-
-p even_array
+array.select{|i| i.even?}
 end
